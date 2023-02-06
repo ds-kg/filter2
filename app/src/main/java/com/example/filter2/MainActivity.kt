bb5d2526797028fa2c6e37754880473e3ac52170
Kenzhebaev Dastan <84958200+ds-kg@users.noreply.github.com>
2023-02-06 19:11:48 +0600

    updated

--- app/src/main/java/com/example/filter2/MainActivity.kt
+++ app/src/main/java/com/example/filter2/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.filter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    val filter = resources.getStringArray(R.array.Filter)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val spinner = findViewById<Spinner>(R.id.spinner1)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, filter
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
     }
 }
