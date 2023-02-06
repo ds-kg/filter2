0f4cf22c0550b3f369bdbdde4efd5642e370af31
Kenzhebaev Dastan <84958200+ds-kg@users.noreply.github.com>
2023-02-06 19:13:34 +0600

    updated

--- app/src/main/java/com/example/filter2/MainActivity.kt
+++ app/src/main/java/com/example/filter2/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val spinner = findViewById<Spinner>(R.id.spinner1)
         if (spinner != null) {
             val adapter = ArrayAdapter(
